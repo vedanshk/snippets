45d3d2c3c01c70ede98212d092203dcb04d29cd4
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2024-12-19 10:52:49 +0530

    add snippet page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import { db } from "@/db";
+export default async function Home() {
 
-export default function Home() {
+   const snippets = await db.snippet.findMany();
+
+   const renderedSnippets = snippets.map(( snippet) => {
+    return(
+      <div key={snippet.id}>
+        {snippet.title}
+      </div>
+    )
+   })
+
   return (
     <div>
      Home Page
+     {renderedSnippets}
     </div>
   );
 }
---
+++ src/app/snippets/[id]/page.tsx
@@ -0,0 +1,24 @@
+
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+interface SnippetShowPageProps {
+    params:{
+        id: string
+    }
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+
+    const { id } = await props.params;
+    const snippet = await db.snippet.findFirst({
+        where: { id: parseInt(id) },
+    });
+    if(!snippet){
+        return notFound();
+    }
+
+    return <div>
+        {snippet.title}
+    </div>
+
+}

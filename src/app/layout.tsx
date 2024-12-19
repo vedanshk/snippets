e15c0902bb6163a61f2c84fc19dcab09fd09c611
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2024-12-19 10:08:31 +0530

    added server action

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container mx-auto px-12">
+            {children}
+        </div>
+        
       </body>
     </html>
   );
